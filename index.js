6f26999743433ff64b33e12938e46bb40c2a732c
markchck <markchck@gmail.com>
2023-07-16 22:12:41 +0900

    feat - signUp, signIn api개발

---
+++ index.js
@@ -0,0 +1,29 @@
+import { signUp, signIn } from "./src/user.js"
+
+export const handler = async (event) => {
+  // console.log(event)
+  let response
+  try {
+    switch (event.routeKey) {
+      case "POST /signUp":
+        response = await signUp(event)
+        break
+      case "POST /signIn":
+        response = await signIn(event)
+        break
+      case "GET /socialLogin":
+        response = await getUserBySocialLogin(event)
+        break
+      default:
+        throw new Error(`Unsupported route: "${event.routeKey}"`)
+    }
+    console.log(response)
+    return response
+  } catch (error) {
+    console.log(error)
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error.message),
+    }
+  }
+}
---
+++ src/user.js
@@ -0,0 +1,34 @@
+import { register, login, socialLogin } from "../lib/firebase.js"
+export const signUp = async (event) => {
+  const body = JSON.parse(event.body.toString("utf-8"))
+  const { userEmail = "", password = "" } = body
+  const user = await register(userEmail, password)
+  return {
+    statusCode: 201,
+    body: user,
+  }
+}
+
+export const signIn = async (event) => {
+  // console.log(event)
+  console.log(process.env)
+  const body = JSON.parse(event.body.toString("utf-8"))
+  const { userEmail = "", password = "" } = body
+  const user = await login(userEmail, password)
+
+  return {
+    statusCode: 200,
+    body: user,
+  }
+}
+
+export const getUserBySocialLogin = async (event) => {
+  const { user = "", token = "" } = await socialLogin()
+  return {
+    statusCode: 200,
+    body: {
+      user: user,
+      token: token,
+    },
+  }
+}
