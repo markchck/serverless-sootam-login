b4d57f90b1edfd55fab0446af9b2feab9204e458
markchck <markchck@gmail.com>
2023-08-07 22:52:45 +0900

    feat - 회원가입 실패시 에러문구 상세화

--- src/user.js
+++ src/user.js
@@ -1,15 +1,21 @@
 import { register, login, socialLogin } from "../lib/firebase.js"
 export const signUp = async (event) => {
-  const body = JSON.parse(event.body.toString("utf-8"))
-  const { userEmail = "", password = "" } = body
-  const user = await register(userEmail, password)
-  return {
-    statusCode: 201,
-    body: user,
+  // console.log(event)
+  try {
+    const body = JSON.parse(event.body.toString("utf-8"))
+    const { userEmail = "", password = "" } = body
+    const response = await register(userEmail, password)
+    return {
+      statusCode: 201,
+      body: response,
+    }
+  } catch (error) {
+    throw error
   }
 }
 
 export const signIn = async (event) => {
+  // console.log(event)
   const body = JSON.parse(event.body.toString("utf-8"))
   const { userEmail = "", password = "" } = body
   const user = await login(userEmail, password)
